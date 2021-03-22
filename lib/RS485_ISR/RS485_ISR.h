ea7187082690ff049328eac7f239a5639b43539c
Interview <35446460+superwsl3@users.noreply.github.com>
2021-03-22 14:48:07 +0800

    20210322_update5
    1.change speed write command function

--- lib/RS485_ISR/RS485_ISR.h
+++ lib/RS485_ISR/RS485_ISR.h
@@ -9,6 +9,13 @@ const byte VFD_CS_Pin = 2;
 VFD RS485_VFD(VFD_CS_Pin);
 //驅動器接收中斷
 
+void test(int a,int b,int &c,int &d){
+    RS485_VFD.VFD_SPEED_Write(a, b);
+    c = L_RPM;
+    d = R_RPM;
+}
+         //寫入轉速至驅動器(指令:0/寫,1/讀;左輪轉速;右輪轉速)
+
 void serialEvent1(){                                         
     RS485_VFD.VFD_DATA_ISR(&L_RPM, &R_RPM);
 }//end void serialEvent1(){
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 #include "RS485_ISR.h"
 int ONCE=0;                                       //1次指令
-
+int a=0;
+int b=0;
 
 
 //驅動器接收資料
 void DEBUG(){
-  Serial.println(L_RPM);        //Speed_RWeel (m/s)
+  Serial.println(a);                          //Speed_RWeel (m/s)
   Serial.println(" ");
 
-  Serial.println(R_RPM);        //Speed_LWeel (m/s)
+  Serial.println(b);                          //Speed_LWeel (m/s)
   Serial.println(" ");										       
 }
 
@@ -20,10 +21,11 @@ void setup(){
 
 void loop() {
   if(ONCE==0){                                    //1次指令
-    RS485_VFD.VFD_SPEED_Write(100, -100);          //寫入轉速至驅動器(指令:0/寫,1/讀;左輪轉速;右輪轉速)
+    //RS485_VFD.VFD_SPEED_Write(100, -100);         //寫入轉速至驅動器(指令:0/寫,1/讀;左輪轉速;右輪轉速)
+    test(200,-300,a,b);
     delay(100);                                   //指令間格時間(delay,不能太快否則導致收區資料異常)
-    //RS485_VFD.VFD_SPEED_Read();   	              //讀取驅動器轉速
-    ONCE=0;
+    //RS485_VFD.VFD_SPEED_Read();   	            //讀取驅動器轉速
+    ONCE=1;
   }            
   DEBUG();
 }
