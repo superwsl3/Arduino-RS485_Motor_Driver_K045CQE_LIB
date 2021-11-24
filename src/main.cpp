9e69d08150926cae865d1ebae9a50fa877b8771b
Interview <35446460+superwsl3@users.noreply.github.com>
2021-11-24 14:27:02 +0800

    update speed limit
    update speed limit

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@ int b=0;
 
 //驅動器接收資料
 void DEBUG(){
-  Serial.println(a);                          //Speed_RWeel (m/s)
+  Serial.println(a);                              //Speed_RWeel (m/s)
   Serial.println(" ");
 
-  Serial.println(b);                          //Speed_LWeel (m/s)
+  Serial.println(b);                              //Speed_LWeel (m/s)
   Serial.println(" ");										       
 }
 
@@ -21,10 +21,10 @@ void setup(){
 
 void loop() {
   if(ONCE==0){                                    //1次指令
-    //RS485_VFD.VFD_SPEED_Write(100, -100);       //寫入轉速至驅動器(指令:0/寫,1/讀;左輪轉速;右輪轉速)
-    test(2000,2000,a,b);
+    //RS485_VFD.VFD_SPEED_Write(100, -100);       //寫  --(左輪轉速命令,右輪轉速命令)
+    test(500,500,a,b);                            //寫讀--(寫左輪轉速,寫右輪轉速,讀左輪轉速,讀右輪轉速)
     delay(100);                                   //指令間格時間(delay,不能太快否則導致收區資料異常)
-    //RS485_VFD.VFD_SPEED_Read();   	            //讀取驅動器轉速
+    //RS485_VFD.VFD_SPEED_Read();   	            //讀  --(讀左輪轉速,讀右輪轉速)
     ONCE=0;
   }            
   DEBUG();
